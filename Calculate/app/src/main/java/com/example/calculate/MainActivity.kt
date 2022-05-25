0316feab775902c36dd00085058b0f216f10d167
nayeongKim <skdud4659@gmail.com>
2022-05-26 01:18:58 +0900

    [+] - project4. Calculator

--- Calculate/app/src/main/java/com/example/calculate/MainActivity.kt
+++ Calculate/app/src/main/java/com/example/calculate/MainActivity.kt
@@ -6,24 +6,38 @@ import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import androidx.room.Room
+import com.example.calculate.model.History
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     private val expressionText: TextView by lazy { findViewById(R.id.expression) }
     private val resultText: TextView by lazy { findViewById(R.id.resultText) }
+    private val historyView: View by lazy { findViewById(R.id.historyLayout) }
+    private val historyViewInner: LinearLayout by lazy { findViewById(R.id.historyInner) }
+
     private var isOps: Boolean = false
     private var hasOps: Boolean = false
+
+    lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "historyDB").build()
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun buttonClicked(view: View) {
+    fun buttonClicked(view: View) {
         when (view.id) {
             R.id.btn0 -> numButtonClicked("0")
             R.id.btn1 -> numButtonClicked("1")
@@ -57,6 +71,7 @@ class MainActivity : AppCompatActivity() {
             return
         }
         expressionText.append(number)
+        resultText.text = calculateExpression()
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -80,7 +95,7 @@ class MainActivity : AppCompatActivity() {
         val ssb = SpannableStringBuilder(expressionText.text)
         ssb.setSpan(
             ForegroundColorSpan(getColor(R.color.green)),
-            expressionText.text.length -1,
+            expressionText.text.length - 1,
             expressionText.text.length,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
@@ -89,15 +104,81 @@ class MainActivity : AppCompatActivity() {
         hasOps = true
     }
 
-    private fun historyButtonClicked(view: View) {
 
+    fun resultButtonClicked(view: View) {
+        val expression = expressionText.text.toString()
+        val result = calculateExpression()
+
+        // db
+        Thread(Runnable {
+            db.historyDao().insertHistory(History(null, expression, result))
+        }).start()
+
+        expressionText.text = resultText.text
+        resultText.text = ""
     }
 
-    private fun resultButtonClicked(view: View) {
+    private fun calculateExpression(): String {
+        val expressionTexts = expressionText.text.split(" ")
+        if (hasOps.not() || expressionTexts.size !== 3) {
+            return ""
+        } else if (expressionTexts[0].isNumber().not() || expressionTexts[2].isNumber().not()) {
+            return ""
+        }
+        val exp1 = expressionTexts[0].toBigInteger()
+        val exp2 = expressionTexts[2].toBigInteger()
+        val op = expressionTexts[1]
 
+        return when (op) {
+            "+" -> (exp1 + exp2).toString()
+            "-" -> (exp1 - exp2).toString()
+            "*" -> (exp1 * exp2).toString()
+            "/" -> (exp1 / exp2).toString()
+            "%" -> (exp1 % exp2).toString()
+            else -> ""
+        }
     }
 
-    private fun clearButtonClicked(view: View) {
+    fun clearButtonClicked(view: View) {
+        expressionText.text = ""
+        resultText.text = ""
+        isOps = false
+        hasOps = false
+    }
 
+    fun historyButtonClicked(view: View) {
+        historyView.isVisible = true
+        historyViewInner.removeAllViews()
+        Thread(Runnable {
+            db.historyDao().getAll().reversed().forEach {
+                runOnUiThread{
+                    val historyView = LayoutInflater.from(this).inflate(R.layout.history_row, null, false)
+                    historyView.findViewById<TextView>(R.id.expression).text = it.expression
+                    historyView.findViewById<TextView>(R.id.resultText).text = "= ${it.result}"
+                    historyViewInner.addView(historyView)
+                }
+            }
+        }).start()
+    }
+
+    fun closeHistoryBtn(view: View) {
+        historyView.isVisible = false
+    }
+
+    fun historyDelBtn(view: View) {
+        historyViewInner.removeAllViews()
+        Thread(Runnable {
+            db.historyDao().delAll()
+        }).start()
+    }
+}
+
+// 확장함수
+fun String.isNumber(): Boolean {
+    return try {
+        this.toBigInteger()
+        true
+    } catch (e: NumberFormatException) {
+        false
     }
 }
