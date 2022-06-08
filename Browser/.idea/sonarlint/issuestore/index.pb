2092aa6d53de1dc2292601d440919b733db70977
nayeong <83805167+skdud4659@users.noreply.github.com>
2022-06-08 23:10:08 +0900

    [+] - Intermediate project2. Quote

---
+++ Browser/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+e
+5app/src/main/java/com/example/browser/MainActivity.kt,1/3/135888045f0193bfa7f08822f153abb721121f7f
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
--- TodaysWiseSaying/app/src/main/java/com/example/todayswisesaying/QuoteAdapter.kt
+++ TodaysWiseSaying/app/src/main/java/com/example/todayswisesaying/QuoteAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.todayswisesaying
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class QuoteAdapter(
-  private val quotes: List<Quote>
+  private val quotes: List<Quote>,
+  private val isNameRevealed: Boolean
 ): RecyclerView.Adapter<QuoteAdapter.QuoteViewHolder>() {
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
     QuoteViewHolder(
@@ -16,19 +18,27 @@ class QuoteAdapter(
     )
   
   override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-    holder.bind(quotes[position])
+    val actualPosition = position % quotes.size
+    holder.bind(quotes[actualPosition], isNameRevealed)
   }
-  
-  override fun getItemCount() = quotes.size
+
+  // 무한 스와이프
+  override fun getItemCount() = Int.MAX_VALUE
   
   
   inner class QuoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val quoteText: TextView = itemView.findViewById(R.id.quote)
     private val nameText: TextView = itemView.findViewById(R.id.name)
     
-    fun bind(quote: Quote) {
-      quoteText.text = quote.quote
-      nameText.text = quote.name
+    @SuppressLint("SetTextI18n")
+    fun bind(quote: Quote, isNameRevealed: Boolean) {
+      quoteText.text = "\"${quote.quote}\""
+      if (isNameRevealed) {
+        nameText.text = "- ${quote.name}"
+        nameText.visibility = View.VISIBLE
+      } else {
+        nameText.visibility = View.GONE
+      }
     }
   }
 }
